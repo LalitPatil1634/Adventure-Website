fcee5aaf7dfaebc50ed8fe58b39cd9ad1639ce40
Lalit LP <lalitpatil44444@gmail.com>
2022-10-25 15:23:07 +0530

    review added on about page

---
+++ src/components/pages/About/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from '../../Footer';
+import { Index } from './Review/Index';
+
+
+export default function About() {
+    return (
+        <>
+            <div>
+                <Index />
+            </div>
+            <Footer />
+        </>
+    )
+}
